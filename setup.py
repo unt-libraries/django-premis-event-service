49a8e95dc5b2b2182368139a61d0ca63d732fbe4
Damon Kelley <damon.kelley@gmail.com>
2015-09-16 16:32:34 -0500

    Change version to 1.0.0 to match organizational versioning scheme.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = "django-premis-event-service",
-    version = "1.0",
+    version = "1.0.0",
     packages = find_packages(),
     include_package_data = True,
     package_data = {

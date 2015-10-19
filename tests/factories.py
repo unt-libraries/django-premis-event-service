9404eac207e2cb4838ed6004689aaae245503023
Damon Kelley <damon.kelley@gmail.com>
2015-10-19 11:56:53 -0500

    New objects to take in an agent or model and generate the xml.

--- tests/factories.py
+++ tests/factories.py
@@ -39,6 +39,27 @@ class AgentFactory(factory.django.DjangoModelFactory):
     agent_note = factory.fuzzy.FuzzyText()
 
 
+class AgentXML(object):
+
+    def __init__(self, agent):
+        self.agent = agent
+
+    @property
+    def xml(self):
+        xml = """<?xml version="1.0"?>
+            <premis:agent xmlns:premis="info:lc/xmlns/premis-v2">
+              <premis:agentIdentifier>
+                <premis:agentIdentifierValue>{agent_identifier}</premis:agentIdentifierValue>
+                <premis:agentIdentifierType>PES:Agent</premis:agentIdentifierType>
+              </premis:agentIdentifier>
+              <premis:agentName>{agent_name}</premis:agentName>
+              <premis:agentType>{agent_type}</premis:agentType>
+              <premis:agentNote>{agent_note}</premis:agentNote>
+            </premis:agent>
+        """
+        return xml.format(**vars(self.agent))
+
+
 class LinkObjectFactory(factory.django.DjangoModelFactory):
 
     class Meta:
@@ -76,3 +97,80 @@ class EventFactory(factory.django.DjangoModelFactory):
         if create and extracted:
             linking_objects = LinkObjectFactory.create_batch(count)
             self.linking_objects.add(*linking_objects)
+
+
+class EventXML(object):
+
+    def __init__(self, event):
+        self.event = event
+
+    @property
+    def entry_xml(self):
+        xml = """<?xml version="1.0"?>
+            <entry xmlns="http://www.w3.org/2005/Atom">
+              <title>{event_identifier}</title>
+              <id>http://example.com/APP/event/{event_identifier}/</id>
+              <updated>2015-10-02T19:59:22Z</updated>
+              <content type="application/xml">
+                <premis:event xmlns:premis="info:lc/xmlns/premis-v2">
+                  <premis:eventDetail>{event_detail}</premis:eventDetail>
+                  <premis:eventOutcomeInformation>
+                    <premis:eventOutcomeDetail>{event_outcome_detail}</premis:eventOutcomeDetail>
+                    <premis:eventOutcome>{event_outcome}</premis:eventOutcome>
+                  </premis:eventOutcomeInformation>
+                  <premis:eventType>{event_type}</premis:eventType>
+                  <premis:linkingAgentIdentifier>
+                    <premis:linkingAgentIdentifierValue>{linking_agent_identifier_value}</premis:linkingAgentIdentifierValue>
+                    <premis:linkingAgentIdentifierType>{linking_agent_identifier_type}</premis:linkingAgentIdentifierType>
+                  </premis:linkingAgentIdentifier>
+                  <premis:eventIdentifier>
+                      <premis:eventIdentifierValue>{event_identifier}</premis:eventIdentifierValue>
+                    <premis:eventIdentifierType>{event_identifier_type}</premis:eventIdentifierType>
+                  </premis:eventIdentifier>
+                  <premis:eventDateTime>{event_date_time}</premis:eventDateTime>
+                  {linking_objects}
+                </premis:event>
+              </content>
+            </entry>
+        """
+        return xml.format(
+                linking_objects=self._linking_objects_xml(),
+                **vars(self.event))
+
+    @property
+    def xml(self):
+        xml = """<?xml version="1.0"?>
+            <premis:event xmlns:premis="info:lc/xmlns/premis-v2">
+              <premis:eventDetail>{event_detail}</premis:eventDetail>
+              <premis:eventOutcomeInformation>
+                <premis:eventOutcomeDetail>{event_outcome_detail}</premis:eventOutcomeDetail>
+                <premis:eventOutcome>{event_outcome}</premis:eventOutcome>
+              </premis:eventOutcomeInformation>
+              <premis:eventType>{event_type}</premis:eventType>
+              <premis:linkingAgentIdentifier>
+                <premis:linkingAgentIdentifierValue>{linking_agent_identifier_value}</premis:linkingAgentIdentifierValue>
+                <premis:linkingAgentIdentifierType>{linking_agent_identifier_type}</premis:linkingAgentIdentifierType>
+              </premis:linkingAgentIdentifier>
+              <premis:eventIdentifier>
+                  <premis:eventIdentifierValue>{event_identifier}</premis:eventIdentifierValue>
+                <premis:eventIdentifierType>{event_identifier_type}</premis:eventIdentifierType>
+              </premis:eventIdentifier>
+              <premis:eventDateTime>{event_date_time}</premis:eventDateTime>
+              {linking_objects}
+            </premis:event>
+        """
+        return xml.format(
+                linking_objects=self._linking_objects_xml(),
+                **vars(self.event))
+
+    def _linking_objects_xml(self):
+        template = """<premis:linkingObjectIdentifier>
+            <premis:linkingObjectIdentifierType>{object_type}</premis:linkingObjectIdentifierType>
+            <premis:linkingObjectIdentifierValue>{object_identifier}</premis:linkingObjectIdentifierValue>
+            <premis:linkingObjectRole>{object_role}</premis:linkingObjectRole>
+        </premis:linkingObjectIdentifier>
+        """
+        xml = ''
+        for obj in self.event.linking_objects.all():
+            xml += template.format(**vars(obj))
+        return xml

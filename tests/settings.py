0d2fbc29ac064c9712d8a1d061792bebdc4c5016
runderwood <reed.underwood@unt.edu>
2017-02-23 09:47:54 -0600

    remove redundant/pointless INTERNAL_IPS list in test settings

--- tests/settings.py
+++ tests/settings.py
@@ -31,7 +31,6 @@ MIDDLEWARE_CLASSES = (
 if os.getenv('PES_DEBUG_TOOLBAR'):
     INSTALLED_APPS += ('debug_toolbar',)
     MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-    INTERNAL_IPS = ['127.0.0.1']
     DEBUG_TOOLBAR_CONFIG = {
         'SHOW_TOOLBAR_CALLBACK': lambda request: True
     }

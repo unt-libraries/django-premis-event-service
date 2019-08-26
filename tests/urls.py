e7239c62261f961f5c0cc0407b9012d7e260625a
madhulika95b <srisudhamadhulika.bayyavarapu@unt.edu>
2019-08-26 11:21:38 -0500

    Update urls to new django style

--- tests/urls.py
+++ tests/urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'', include('premis_event_service.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
 
 if settings.DEBUG and os.getenv('PES_DEBUG_TOOLBAR'):

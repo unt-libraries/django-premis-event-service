2714fac92f3b727d6fbcd6216426aa5d38c3bffb
madhulika95b <srisudhamadhulika.bayyavarapu@unt.edu>
2019-10-14 17:40:14 -0500

    Remove unicode from models

--- premis_event_service/models.py
+++ premis_event_service/models.py
@@ -36,9 +36,6 @@ class Agent(models.Model):
         help_text="Optional note about agent."
     )
 
-    def __unicode__(self):
-        return self.agent_name
-
     def get_absolute_url(self):
         return reverse('agent-detail', args=[self.agent_identifier])
 
@@ -66,9 +63,6 @@ class LinkObject(models.Model):
         help_text="A high-level characterization of the role of the object.",
         null=True
     )
-
-    def __unicode__(self):
-        return self.object_identifier
 
 
 class EventManager(models.Manager):
@@ -211,9 +205,6 @@ class Event(models.Model):
         through='EventLinkObject',
         through_fields=('event_id', 'linkobject_id')
     )
-
-    def __unicode__(self):
-        return self.event_identifier
 
     class Meta:
         ordering = ["event_added"]

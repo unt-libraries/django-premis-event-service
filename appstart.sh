90808c613c6477aa27512dbea6f538566f3bb0cc
clarktr1 <itreyclark@gmail.com>
2026-02-10 18:41:06 -0600

    remove pyenv and extra files to add pyproject

--- appstart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/wait-for-mysqld.sh
-echo "Migrate..."
-pipenv run python manage.py migrate --settings=tests.settings
-echo "Start app..."
-pipenv run python manage.py runserver 0.0.0.0:80 --settings=tests.settings
